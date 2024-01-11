4ca21bb0b2ba771c41028a7cf619b31dff1b3dea
irene <panisirene@gmail.com>
2024-01-11 14:18:02 -0800

    set up server connection

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const db = require('./src/config/connection');
+const routes = require('./src/controllers');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}!`);
+  });
+});
