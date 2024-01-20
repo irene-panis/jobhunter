a4275f4b69d4e7d632d66212596e941d72acc51d
irene <panisirene@gmail.com>
2024-01-19 20:20:24 -0800

    implement switching register/login form using state

--- src/views/components/Login.jsx
+++ src/views/components/Login.jsx
@@ -1,51 +1,51 @@
 
-function Login() {
+export const Login = (props) => {
   return (
-    <div className="flex justify-evenly w-full self-center">
-    <div className="flex flex-col gap-5">
-      <h2 className="text-3xl">Register</h2>
+    <div className="formContainer flex flex-col gap-5">
+      <h2 className="text-3xl">Login</h2>
       <form id="registrationForm" className="flex flex-col gap-5">
-        <div className="flex gap-10">
-          <div className="flex flex-col gap-1">
-            <label className="text-xs uppercase" htmlFor="firstName">First Name</label>
-            <input className="rounded-md text-black pl-2" type="text" id="firstName" name="firstName" required />
-          </div>
-          <div className="flex flex-col gap-1">
-            <label className="text-xs uppercase" htmlFor="email">Email</label>
-            <input className="rounded-md text-black pl-2" type="email" id="email" name="email" required />
-          </div>
-        </div>
-
-        <div className="flex gap-10">
-          <div className="flex flex-col gap-1">
-            <label className="text-xs uppercase" htmlFor="password">Password</label>
-            <input className="rounded-md text-black pl-2" type="password" id="password" name="password" required />
-          </div>
-          <div className="flex flex-col gap-1">
-            <label className="text-xs uppercase" htmlFor="confirmPassword">Confirm Password</label>
+        <div className="inputContainer flex gap-10">
+          <div className="emailContainer flex flex-col gap-1">
+            <label className="text-xs uppercase" htmlFor="email">
+              Email
+            </label>
             <input
               className="rounded-md text-black pl-2"
-              type="password"
-              id="confirmPassword"
-              name="confirmPassword"
+              type="email"
+              id="email"
+              name="email"
               required
             />
           </div>
         </div>
 
-        <div className="flex justify-end">
-          <button type="button" onClick="register()" className="bg-[#415A77] hover:bg-[#778DA9] ease-in-out duration-300 py-2 px-3 rounded-md">
-            Register
+        <div className="inputContainer flex gap-10">
+          <div className="passwordContainer flex flex-col gap-1">
+            <label className="text-xs uppercase" htmlFor="password">
+              Password
+            </label>
+            <input
+              className="rounded-md text-black pl-2"
+              type="password"
+              id="password"
+              name="password"
+              required
+            />
+          </div>
+        </div>
+
+        <div className="buttonContainer flex">
+          <button
+            type="button"
+            className="bg-[#415A77] hover:bg-[#778DA9] ease-in-out duration-300 py-2 px-3 rounded-md"
+          >
+            Login
           </button>
+        </div>
+        <div className="switchFormContainer flex justify-end">
+          <span>Don't have an account? <button onClick={() => props.onFormSwitch("register")} className="underline decoration-solid hover:bg-off-white hover:text-black ease-in-out duration-300">Register</button></span>
         </div>
       </form>
     </div>
-    <div className="">
-      <h2 className="text-4xl">Your job hunt just got a whole lot easier.</h2>
-      <h3 className="text-xl">Register now to start tracking your applications.</h3>
-    </div>
-    </div>
   );
 }
-
-export default Login;
--- src/views/components/Register.jsx
+++ src/views/components/Register.jsx
@@ -1,28 +1,52 @@
-
-function Register() {
+export const Register = (props) => {
   return (
-    <div className="flex justify-evenly w-full self-center">
-    <div className="flex flex-col gap-5">
+    <div className="formContainer flex flex-col gap-5">
       <h2 className="text-3xl">Register</h2>
       <form id="registrationForm" className="flex flex-col gap-5">
-        <div className="flex gap-10">
-          <div className="flex flex-col gap-1">
-            <label className="text-xs uppercase" htmlFor="firstName">First Name</label>
-            <input className="rounded-md text-black pl-2" type="text" id="firstName" name="firstName" required />
+        <div className="inputContainer flex gap-10">
+          <div className="nameContainer flex flex-col gap-1">
+            <label className="text-xs uppercase" htmlFor="firstName">
+              First Name
+            </label>
+            <input
+              className="rounded-md text-black pl-2"
+              type="text"
+              id="firstName"
+              name="firstName"
+              required
+            />
           </div>
-          <div className="flex flex-col gap-1">
-            <label className="text-xs uppercase" htmlFor="email">Email</label>
-            <input className="rounded-md text-black pl-2" type="email" id="email" name="email" required />
+          <div className="emailContainer flex flex-col gap-1">
+            <label className="text-xs uppercase" htmlFor="email">
+              Email
+            </label>
+            <input
+              className="rounded-md text-black pl-2"
+              type="email"
+              id="email"
+              name="email"
+              required
+            />
           </div>
         </div>
 
-        <div className="flex gap-10">
-          <div className="flex flex-col gap-1">
-            <label className="text-xs uppercase" htmlFor="password">Password</label>
-            <input className="rounded-md text-black pl-2" type="password" id="password" name="password" required />
+        <div className="inputContainer flex gap-10">
+          <div className="passwordContainer flex flex-col gap-1">
+            <label className="text-xs uppercase" htmlFor="password">
+              Password
+            </label>
+            <input
+              className="rounded-md text-black pl-2"
+              type="password"
+              id="password"
+              name="password"
+              required
+            />
           </div>
-          <div className="flex flex-col gap-1">
-            <label className="text-xs uppercase" htmlFor="confirmPassword">Confirm Password</label>
+          <div className="confirmContainer flex flex-col gap-1">
+            <label className="text-xs uppercase" htmlFor="confirmPassword">
+              Confirm Password
+            </label>
             <input
               className="rounded-md text-black pl-2"
               type="password"
@@ -33,19 +57,18 @@ function Register() {
           </div>
         </div>
 
-        <div className="flex justify-end">
-          <button type="button" onClick="register()" className="bg-[#415A77] hover:bg-[#778DA9] ease-in-out duration-300 py-2 px-3 rounded-md">
+        <div className="buttonContainer flex">
+          <button
+            type="button"
+            className="bg-[#415A77] hover:bg-[#778DA9] ease-in-out duration-300 py-2 px-3 rounded-md"
+          >
             Register
           </button>
         </div>
+        <div className="buttonContainer flex">
+          <span>Already have an account? <button onClick={() => props.onFormSwitch('login')} className="underline decoration-solid hover:bg-off-white hover:text-black ease-in-out duration-300">Sign in</button></span>
+        </div>
       </form>
-    </div>
-    <div className="">
-      <h2 className="text-4xl">Your job hunt just got a whole lot easier.</h2>
-      <h3 className="text-xl">Register now to start tracking your applications.</h3>
-    </div>
     </div>
   );
 }
-
-export default Register;
