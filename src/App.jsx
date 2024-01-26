7e29bcb5342770214e0f15dde7c1fe6e4f099a7c
irene <panisirene@gmail.com>
2024-01-25 19:26:24 -0800

    add conditional rendering of pages based on logged in/out

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
 import Landing from './views/pages/Landing';
+import { Navbar } from './views/components/Navbar';
+import AuthService from './utils/decode';
 
 function App() {
   return (
     <>
       <main className="flex min-h-screen bg-dark-mode text-off-white font-montserrat">
-        <Landing/>
+        {
+          AuthService.loggedIn() ?
+          <Navbar/> :
+          <Landing/>
+        }
       </main>
     </>
   )
--- src/views/components/Navbar.jsx
+++ src/views/components/Navbar.jsx
@@ -1,13 +1,22 @@
+import AuthService from '../../utils/decode';
+
 export const Navbar = () => {
   return (
-    <div className="navContainer">
-      <h1>JobTracker</h1>
-      <ul>
+    <div className="navContainer flex flex-col w-1/5 bg-[#1a1625] py-10 px-20 gap-20 text-2xl lowercase">
+      <h1 className="font-bold">JobTracker</h1>
+      <ul className="flex flex-col gap-10">
         <li>Overview</li>
         <li>Applications</li>
         <li>Interviews</li>
         <li>Settings</li>
       </ul>
+      <button 
+        type="button" 
+        onClick={AuthService.logout}
+        className="lowercase text-left"
+      >
+        Logout
+      </button>
     </div>
   );
 }
