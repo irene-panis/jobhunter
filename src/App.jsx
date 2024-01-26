4601ac138459b7cb3b58041974d89a011c0f1280
irene <panisirene@gmail.com>
2024-01-26 14:15:16 -0800

    incorporate react router and error page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { Outlet } from 'react-router-dom';
 import Landing from './views/pages/Landing';
 import { Navbar } from './views/components/Navbar';
 import AuthService from './utils/decode';
@@ -6,15 +7,20 @@ import AuthService from './utils/decode';
 function App() {
   return (
     <>
-      <main className="flex min-h-screen bg-dark-mode text-off-white font-montserrat">
-        {
-          AuthService.loggedIn() ?
-          <Navbar/> :
+      {
+        AuthService.loggedIn() ? (
+          <div>
+            <Navbar/>
+            <main className="flex min-h-screen bg-dark-mode text-off-white font-montserrat">
+              <Outlet/>
+            </main>     
+          </div>
+        ) : (
           <Landing/>
-        }
-      </main>
+        )
+      }
     </>
   )
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import './App.css'
+
+import App from './App';
+import Overview from './views/pages/Overview';
+import Applications from './views/pages/Applications';
+import Interviews from './views/pages/Interviews';
+import Settings from './views/pages/Settings';
+import Error from './views/pages/Error';
+
+// Define the accessible routes, and which components respond to which URL
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Overview />,
+      },
+      {
+        path: '/Applications',
+        element: <Applications />,
+      },
+      {
+        path: '/Interviews',
+        element: <Interviews />,
+      },
+      {
+        path: '/Settings',
+        element: <Settings />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <RouterProvider router={router} />
+);
