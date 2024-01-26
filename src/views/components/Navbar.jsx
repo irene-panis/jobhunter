f2c0faef66e4067de0fbcb68a436ed170747d0d4
irene <panisirene@gmail.com>
2024-01-26 14:33:35 -0800

    include link elements in navbar for routing

--- src/views/components/Navbar.jsx
+++ src/views/components/Navbar.jsx
@@ -1,14 +1,31 @@
 import AuthService from '../../utils/decode';
+import { Link } from 'react-router-dom';
 
 export const Navbar = () => {
   return (
     <div className="navContainer flex flex-col w-1/5 bg-[#1a1625] py-10 px-20 gap-20 text-2xl lowercase">
       <h1 className="font-bold">JobTracker</h1>
       <ul className="flex flex-col gap-10">
-        <li>Overview</li>
-        <li>Applications</li>
-        <li>Interviews</li>
-        <li>Settings</li>
+        <li>
+          <Link to="/">
+            Overview
+          </Link>
+        </li>
+        <li>
+          <Link to="/Applications">
+            Applications
+          </Link>
+        </li>
+        <li>
+          <Link to="/Interviews">
+           Interviews
+          </Link>
+        </li>
+        <li>
+          <Link to="/Settings">
+            Settings
+          </Link>
+        </li>
       </ul>
       <button 
         type="button" 
