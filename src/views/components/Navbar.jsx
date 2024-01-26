c0b20428a00accab5cedbb94b71f61334f0ebe0d
irene <panisirene@gmail.com>
2024-01-25 18:42:51 -0800

    build navbar skeleton

---
+++ src/views/components/Navbar.jsx
@@ -0,0 +1,13 @@
+export const Navbar = () => {
+  return (
+    <div className="navContainer">
+      <h1>JobTracker</h1>
+      <ul>
+        <li>Overview</li>
+        <li>Applications</li>
+        <li>Interviews</li>
+        <li>Settings</li>
+      </ul>
+    </div>
+  );
+}
