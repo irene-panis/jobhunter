5318bc8ae1182af0f891b7b2ace06b8b34918cb5
irene <panisirene@gmail.com>
2024-01-30 19:02:06 -0800

    add styling

--- src/views/components/JobContainer.jsx
+++ src/views/components/JobContainer.jsx
@@ -1,7 +1,7 @@
 export const JobContainer = ({ job }) => {
   return (
     <div className="flex flex-col bg-white text-black rounded-md">
-      <p>{ job.position }</p>
+      <p className="font-bold">{ job.position }</p>
       <p>{ job.company }</p>
       <p>{ job.location }</p>
       <p>{ job.notes }</p>
