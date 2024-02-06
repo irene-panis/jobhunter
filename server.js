4cee0775ee126d3ec46b28ffd3677f97145dd20a
irene <panisirene@gmail.com>
2024-02-05 19:59:58 -0800

    add path

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import connection from './src/config/connection.js';
 import routes from './src/routes/index.js';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
 dotenv.config();
 
 const app = express();
@@ -13,6 +14,14 @@ app.use(express.json());
 app.use(cors());
 
 app.use(routes);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../dist/index.html'));
+  });
+}
 
 connection.once('open', () => {
   app.listen(PORT, () => {
