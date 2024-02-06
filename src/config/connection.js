34ad9d33a57ef10b4a04694e93420a33758b689b
irene <panisirene@gmail.com>
2024-02-05 18:54:31 -0800

    deploying

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
 
-mongoose.connect('mongodb://127.0.0.1:27017/jobtrackerDB');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/jobtrackerDB');
 
 export default mongoose.connection;
