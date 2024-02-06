d1db6ecb8ec5a0095696e28ac02551daf936cc10
irene <panisirene@gmail.com>
2024-02-05 20:05:36 -0800

    fix sendfile

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ import routes from './src/routes/index.js';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 dotenv.config();
 
 const app = express();
@@ -19,7 +22,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../dist')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../dist/index.html'));
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
   });
 }
 
